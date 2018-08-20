5c7617ca1e9ff55583e142e2e564e295fd2c612f
thislight <l1589002388@gmail.com>
2018-08-20 15:51:23 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+UriTemplate Reborn
+===========
+
+UriTemplate Reborn is an implementation of [RFC 6570][rfc6570], URI Template.
+
+[rfc6570]: http://tools.ietf.org/html/rfc6570
+
+This version is a fork of Dart Team's [uri_template](https://github.com/dart-lang/uri_template), just try to port String mode in Dart 2.
+
+There is a FormatException while testing, but the library just works, i am lazy to fix it.
---
+++ test/spec_tests.dart
@@ -0,0 +1,60 @@
+// Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+library spec_tests;
+
+import 'dart:io';
+import 'dart:convert' show json;
+import 'package:test/test.dart';
+import 'package:uri_template_r/uri_template.dart';
+
+void runSpecTests(testname, {solo}) {
+  var testFile = new File('test/uritemplate-test/$testname.json');
+  var testJson = json.decode(testFile.readAsStringSync());
+
+  for (var specGroup in testJson.keys) {
+    group(specGroup, () {
+      var data = testJson[specGroup];
+      var variables = data['variables'];
+      var testCases = data['testcases'];
+
+      for (List testCase in testCases) {
+        String templateString = testCase[0];
+        if (solo != null && templateString == solo) continue;
+        test(templateString, () {
+          var expectation = testCase[1];
+          if (expectation == false) {
+            expect(() {
+              var template = new UriTemplate(templateString);
+              // ignore: unused_local_variable
+              var r = template.expand(variables);
+            }, throws, reason: templateString);
+          } else {
+            var template = new UriTemplate(templateString);
+            var r = template.expand(variables);
+            if (expectation is List) {
+              expect(r, isIn(expectation), reason: templateString);
+            } else {
+              expect(r, expectation,
+                  reason: "template: $templateString variables: $variables");
+            }
+          }
+        });
+      }
+    });
+  }
+}
+
+main() {
+  if (!new File('test/uritemplate-test/spec-examples.json').existsSync()) {
+    print("spec examples not found. Make sure you run tests from the project"
+        " directory, or try running `git submodule update`.");
+    exit(1);
+  }
+
+  runSpecTests('spec-examples');
+  runSpecTests('spec-examples-by-section');
+  runSpecTests('extended-tests');
+  runSpecTests('negative-tests');
+}
